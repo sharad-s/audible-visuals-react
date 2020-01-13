4760e87c8eb74176e0e71c6acd9fa96a36bf0e21
sharad-pizza-hut <sharad.shekar@yum.com>
2020-01-13 00:22:06 -0600

    refactor

--- src/utils/getAnalyser.js
+++ src/utils/getAnalyser.js
@@ -3,6 +3,7 @@ import isEmpty from './isEmpty';
 var ctx = new (window.AudioContext || window.webkitAudioContext)(); // creates audioNode
 var analyser = ctx.createAnalyser(); // creates analyserNode
 
+// Required to patch getFloatTimeDomainData for Safari
 if (AnalyserNode && isEmpty(AnalyserNode.prototype.getFloatTimeDomainData)) {
   var uint8 = new Uint8Array(2048);
   AnalyserNode.prototype.getFloatTimeDomainData = function(array) {
--- src/utils/isEmpty.js
+++ src/utils/isEmpty.js
@@ -1,7 +1,5 @@
-const isEmpty = value =>
+export default value =>
   value === undefined ||
   value === null ||
   (typeof value === 'object' && Object.keys(value).length === 0) ||
   (typeof value === 'string' && value.trim().length === 0);
-
-export default isEmpty;
